d12907dd681c1d16c623b9dcceed9ff5e85c2ac6
Michael Kelly <mkelly@mozilla.com>
2015-04-24 14:43:35 -0700

    Use X-Frame-Options decorator to override middleware.

--- views.py
+++ views.py
@@ -1,9 +1,8 @@
-
 from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_sameorigin
 
 
+@xframe_options_sameorigin
 def intro(request, template='intro.html'):
     response = render(request, template)
-    response['X-Frame-Options'] = 'SAMEORIGIN'
-
     return response
