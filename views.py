226c168ed22d2ac4153c9c9bca73fb3059f9d63a
Matjaž Horvat <matjaz.horvat@gmail.com>
2015-11-27 14:30:48 +0100

    Remove localizability support

--- views.py
+++ views.py
@@ -3,6 +3,5 @@ from django.views.decorators.clickjacking import xframe_options_sameorigin
 
 
 @xframe_options_sameorigin
-def intro(request, template='intro.html'):
-    response = render(request, template)
-    return response
+def intro(request):
+    return render(request, 'intro.html')
