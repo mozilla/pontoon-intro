8534a40110aaf3f86d2a1056109502fa706d2e68
Matjaž Horvat <matjaz.horvat@gmail.com>
2018-08-23 17:48:51 +0200

    Only keep localizable files

--- views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views.decorators.clickjacking import xframe_options_sameorigin
-
-
-@xframe_options_sameorigin
-def intro(request):
-    return render(request, 'intro.html')
